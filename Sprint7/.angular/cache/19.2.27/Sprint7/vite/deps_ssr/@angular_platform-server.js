e5814a6472ce7240518034401e43d96998644bef
aJeaneSilva <jeaneps.contact@gmail.com>
2026-06-18 08:43:57 -0300

    primeiro commit

---
+++ Sprint7/.angular/cache/19.2.27/Sprint7/vite/deps_ssr/@angular_platform-server.js
@@ -0,0 +1,43 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BEFORE_APP_SERIALIZED,
+  DominoAdapter,
+  ENABLE_DOM_EMULATION,
+  INITIAL_CONFIG,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS,
+  PlatformState,
+  SERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS,
+  ServerModule,
+  VERSION,
+  isHostAllowed,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderInternal,
+  renderModule
+} from "./chunk-DEC57TI4.js";
+import "./chunk-EMZN5RIZ.js";
+import "./chunk-DBRGW57M.js";
+import "./chunk-EVELF3WG.js";
+import "./chunk-CQRODE7T.js";
+import "./chunk-WNXFQNHD.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  BEFORE_APP_SERIALIZED,
+  INITIAL_CONFIG,
+  PlatformState,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule,
+  DominoAdapter as ɵDominoAdapter,
+  ENABLE_DOM_EMULATION as ɵENABLE_DOM_EMULATION,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS,
+  SERVER_CONTEXT as ɵSERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS,
+  isHostAllowed as ɵisHostAllowed,
+  renderInternal as ɵrenderInternal
+};
---
+++ Sprint7/.angular/cache/19.2.27/Sprint7/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-LSWUT3OO.js";
+import "./chunk-E3DUJFMA.js";
+import "./chunk-DEC57TI4.js";
+import "./chunk-EMZN5RIZ.js";
+import "./chunk-DBRGW57M.js";
+import "./chunk-EVELF3WG.js";
+import "./chunk-CQRODE7T.js";
+import "./chunk-WNXFQNHD.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
