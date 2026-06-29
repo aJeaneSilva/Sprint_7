d345a2640bbb4a5e50fdd02596c17384615aa9b0
aJeaneSilva <jeaneps.contact@gmail.com>
2026-06-29 10:47:45 -0300

    Estilizando o boas-vindas

--- Sprint7/src/app/components/boas-vindas/boas-vindas.component.ts
+++ Sprint7/src/app/components/boas-vindas/boas-vindas.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-boas-vindas',
+  standalone: true,
   imports: [],
   templateUrl: './boas-vindas.component.html',
   styleUrl: './boas-vindas.component.css'
 })
 export class BoasVindasComponent {
-
+ router = inject(Router)
+ irParaDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
 }
