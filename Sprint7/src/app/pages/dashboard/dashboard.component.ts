3da0de3eb9668b4b7c68e166881b31fb3352b9c6
aJeaneSilva <jeaneps.contact@gmail.com>
2026-06-29 13:40:27 -0300

    Primeiras aplicações para o dashboard

--- Sprint7/src/app/pages/dashboard/dashboard.component.ts
+++ Sprint7/src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../components/menu/menu.component';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MenuComponent, CardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
