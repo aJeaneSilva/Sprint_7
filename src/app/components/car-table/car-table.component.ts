b595b6ecbc513c5f1963424b914beaa4f10de94c
aJeaneSilva <jeaneps.contact@gmail.com>
2026-06-30 22:15:16 -0300

    Atualizando dashboard

---
+++ src/app/components/car-table/car-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-table',
+  imports: [],
+  templateUrl: './car-table.component.html',
+  styleUrl: './car-table.component.css'
+})
+export class CarTableComponent {
+
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { CarTableComponent } from "../../components/car-table/car-table.component";
+import { DashboardService } from '../../services/dashboard.service';
+import { Veiculo } from '../../models/car';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CardComponent],
+  imports: [CardComponent, CarTableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  dashboardService = inject(DashboardService)
+
+  veiculos: Veiculo[] = []
+  veiculoSelecionado: Veiculo = {
+    id: -1,
+    vehicle: "",
+    volumeTotal: 0,
+    connected: 0,
+    softwareUpdates: 0,
+    img: "",
+    vin: "",
+  }
+
+  ngOnInit(){
+    this.dashboardService.getVeiculos().subscribe({
+      error: () => {},  /*lembrete para colocar possível erro*/
+      next: (veiculos) => {
+        this.veiculos = Object.values(veiculos) as  Veiculo[]
+        this.veiculoSelecionado = this.veiculos[0]
+      }
+    })
+  }
 
 }
